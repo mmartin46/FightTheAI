efa68fa580594a3b6b4214a12d416f12f7616145
Martin <mmartin46@angelo.edu>
2023-07-19 18:39:52 -0500

    Added Changes

--- main.cpp
+++ main.cpp
@@ -17,6 +17,20 @@ Try to make a shooter similar
 to the one at Children's Lighthouse
 
 */
+
+typedef struct {
+    int x;
+    int y;
+    int dx;
+    int dy;
+} Movement;
+
+void eventHandler(int &done)
+{
+
+}
+
+
 int main(int argc, char **argv)
 {
     SDL_Window *window = NULL;
@@ -41,9 +55,60 @@ int main(int argc, char **argv)
 
     int done = 0;
 
+    Movement mov;
+    mov.x = 0;
+    mov.y = 0;
+
+    SDL_Event event;
     while (!done)
     {
 
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_WINDOWEVENT_CLOSE:
+                {
+                    if (window)
+                    {
+                        SDL_DestroyWindow(window);
+                        window = NULL;
+                        done = 1;
+                    }
+                }
+                break;
+                case SDL_QUIT:
+                {
+                    done = 1;
+                }
+                break;
+                case SDL_KEYDOWN:
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_ESCAPE:
+                            done = 1;
+                        break;
+                        case SDLK_UP: {
+                            mov.y += 1;
+                        }
+                        break;
+                        case SDLK_DOWN: {
+                            mov.y -= 1;
+                        }
+                        break;
+                        case SDLK_LEFT: {
+                            mov.x -= 1;
+                        }
+                        break;
+                        case SDLK_RIGHT: {
+                            mov.x += 1;
+                        }
+                    }
+                }
+            }
+        }
+        std::cout << mov.x << " " << mov.y << std::endl;
     }
 
     SDL_DestroyWindow(window);

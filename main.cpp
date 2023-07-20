bf21cec0f2d822a2af661f47125c1bc3fc719b50
Martin <mmartin46@angelo.edu>
2023-07-19 19:18:17 -0500

    Added changes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <SDL2\SDL.h>
 #include <SDL2\SDL_image.h>
 #include <SDL2\SDL_mixer.h>
@@ -19,26 +20,45 @@ to the one at Children's Lighthouse
 */
 
 
-class Player {
-    
+/*
+A simple Entity Class
+*/
+class Entity 
+{
+    private:
+      /* data */
+      float x, y;
+      float dx, dy;
+      int frame;
+      std::vector<SDL_Texture*> sprites;
+    public:
+        Entity(float x, float y, int size);
+        inline float get_x() { return x; }
+        inline void set_x(float v) { x = v; }
+        inline float get_y() { return y; }
+        inline void set_y(float v) { y = v; }
+        inline float get_dx() { return dx; }
+        inline void set_dx(float v) { dx = v; }
+        inline float get_dy() { return dy; }
+        inline void set_dy(float v) { dy = v; }
+
+        inline void setSprite(int idx, SDL_Texture* sprite) { sprites.at(idx) = sprite; }
+        inline SDL_Texture* getSprite(int idx) { return sprites.at(idx); }
+
+        inline void setFrame(int f) { frame = f; }
+        inline int getFrame() { return frame; }
 };
 
-class Movement {
-    private:
-        int x;
-        int y;
-        int dx;
-        int dy;
-    public:
-        inline int get_x() { return x; }
-        inline void set_x(int v) { x = v; }
-        inline int get_y() { return y; }
-        inline void set_y(int v) { y = v; }
-        inline int get_dx() { return dx; }
-        inline void set_dx(int v) { dx = v; }
-        inline int get_dy() { return dy; }
-        inline void set_dy(int v) { dy = v; }
+Entity::Entity(float x, float y, int size)
+{
+    this->set_x(x);
+    this->set_y(y);
+    sprites = std::vector<SDL_Texture*>(size);
+    this->setFrame(0);
 };
+
+
+
 
 class Game
 {
@@ -51,6 +71,7 @@ class Game
 
 int main(int argc, char **argv)
 {
+    Game game;
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
 
@@ -66,14 +87,15 @@ int main(int argc, char **argv)
     );
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    
+    game.set_renderer(renderer);
+
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
     int done = 0;
 
-    Movement mov;
+    Entity mov(0, 0, 1);
     mov.set_x(0);
     mov.set_y(0);
 
@@ -104,8 +126,9 @@ int main(int argc, char **argv)
                 {
                     switch (event.key.keysym.sym)
                     {
-                        case SDLK_ESCAPE:
+                        case SDLK_ESCAPE: {
                             done = 1;
+                        }
                         break;
                         case SDLK_UP: {
                             mov.set_y(mov.get_y() - 1);
@@ -120,8 +143,9 @@ int main(int argc, char **argv)
                         }
                         break;
                         case SDLK_RIGHT: {
-                            mov.set_y(mov.get_y() + 1);
+                            mov.set_x(mov.get_x() + 1);
                         }
+                        break;
                     }
                 }
             }
@@ -130,6 +154,6 @@ int main(int argc, char **argv)
     }
 
     SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
+    SDL_DestroyRenderer(game.get_renderer());
     return 0;
 }

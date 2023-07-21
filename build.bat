1cb413bd8d8da91378555fb983976e87609e0d89
Martin <mmartin46@angelo.edu>
2023-07-21 16:38:33 -0500

    General Changes

---
+++ build.bat
@@ -0,0 +1,9 @@
+if %1 == "build" (
+    g++ -I src/include -L src/lib -o main.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
+    main.exe
+)
+else (
+    git add .
+    git commit -m "General Changes"
+    git push -u origin
+)

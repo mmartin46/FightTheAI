6f39f95750b401c0c3744dd0d9a5f86476ae6013
Martin <mmartin46@angelo.edu>
2023-07-21 16:39:27 -0500

    General Changes

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-if %1 == "build" (
+set arg = %1
+if arg == "build" (
     g++ -I src/include -L src/lib -o main.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
     main.exe
 )

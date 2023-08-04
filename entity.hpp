aef7ccd3f81dad0e382d6e8e5cd083ddf98c2d8f
mmartin46 <mmartin46@angelo.edu>
2023-08-04 11:30:29 -0500

    General commit

--- entity.hpp
+++ entity.hpp
@@ -15,26 +15,26 @@ class Entity
     public:
         Entity();
         Entity(float x, float y, unsigned int size);
-        inline float get_x() { return x; }
-        inline void set_x(float v) { x = v; }
-        inline float get_y() { return y; }
-        inline void set_y(float v) { y = v; }
-        inline float get_dx() { return dx; }
-        inline void set_dx(float v) { dx = v; }
-        inline float get_dy() { return dy; }
-        inline void set_dy(float v) { dy = v; }
+        inline virtual float get_x() { return x; }
+        inline virtual void set_x(float v) { x = v; }
+        inline virtual float get_y() { return y; }
+        inline virtual void set_y(float v) { y = v; }
+        inline virtual float get_dx() { return dx; }
+        inline virtual void set_dx(float v) { dx = v; }
+        inline virtual float get_dy() { return dy; }
+        inline virtual void set_dy(float v) { dy = v; }
 
-        inline int get_w() { return w; }
-        inline void set_w(int v) { w = v; }
-        inline int get_h() { return h; }
-        inline void set_h(int v) { h = v; }
+        inline virtual int get_w() { return w; }
+        inline virtual void set_w(int v) { w = v; }
+        inline virtual int get_h() { return h; }
+        inline virtual void set_h(int v) { h = v; }
 
         virtual void setTexture(int idx, SDL_Texture *texture);
         inline SDL_Texture* getTexture(int idx) { return textures.at(idx); }
 
 
-        inline void setFrame(int f) { frame = f; }
-        inline int getFrame() { return frame; }
+        inline virtual void setFrame(int f) { frame = f; }
+        inline virtual int getFrame() { return frame; }
 };
 
 
